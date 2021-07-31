0734121dc37f4a2f9b576069430de011c46aed1d
moueza <>
2021-07-31 10:54:51 +0200

    resolves #4 p2 Executing our first Test Script using Selenium WebDriver ; see #1 gen exec OK tested

--- SELENIUM-beginners-manualJARsex/FirstWebDriverProject/src/firstPackage/MyFirstTestClass.java
+++ SELENIUM-beginners-manualJARsex/FirstWebDriverProject/src/firstPackage/MyFirstTestClass.java
@@ -1,18 +1,21 @@
 package firstPackage;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class MyFirstTestClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
-
-System.setProperty("webdriver.gecko.driver","E:\\Softwares\\geckodriver.exe”);        	
-WebDriver driver = new FirefoxDriver();
-        	driver.get("https://www.google.com/");
-        	driver.manage().window().maximize();
-        	Thread.sleep(5000);
-        	driver.quit();
+	public static void main(String[] args) throws InterruptedException {
+		/**
+		 * System.setProperty("webdriver.gecko.driver","E:\\Softwares\\geckodriver.exe”);
+		 */
+		System.setProperty("webdriver.gecko.driver",
+				"/home/peter/POUB/lambdatest-selenium-webdriver-tutorial-with-examples/geckodriver-v0.29.1-linux64/geckodriver"); // Linux
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(5000);/** InterruptedException */
+		driver.quit();
 	}
 
 }
